ed2e903427f18a6211cceba5eafb6313151f0306
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2018-05-30 21:24:34 -0400

    character scriptable object

---
+++ SHTANK/Assets/Scripts/Character.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class Character : ScriptableObject
+{
+	[Header("Name and description")]
+	public new string name;
+	[TextArea(3, 10)]
+	public string description;
+
+	[Header("Character stats, mouse over for description")]
+	public Stats stats;
+
+	[Header("Default sprite for debugging")]
+	public Sprite defaultSprite;
+}
+
+[System.Serializable]
+public class Stats
+{
+	[Tooltip("If a character's HP hits 0, they die")]
+	public int hp;
+	[Tooltip("A measure of a character's strength")]
+	public int pow;
+	[Tooltip("A measure of a character's endurance when taking hits")]
+	public int def;
+	[Tooltip("How quick a character is")]
+	public int spd;
+	[Tooltip("How many spaces a character can move in combat")]
+	public int mov;
+}
