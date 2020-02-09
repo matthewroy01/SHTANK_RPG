522b0c339bcfa8e23d8248dad8153118fb14077c
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2020-02-09 02:48:46 -0500

    basically ready for first QA!

---
+++ Player Phases/Player Phases/Assets/Scripts/UtilityOpenURL.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilityOpenURL : MonoBehaviour
+{
+    public string url;
+    public bool closeBuild;
+
+    public void OpenURL()
+    {
+        Application.OpenURL(url);
+
+        if (closeBuild)
+        {
+            Application.Quit();
+        }
+    }
+}
