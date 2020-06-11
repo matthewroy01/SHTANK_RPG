e8600c98cbe3b5e97acc0b50f3c20a465a75be6b
Matthew Roy <matthroy09@gmail.com>
2020-06-11 00:27:38 -0400

    context sensitive UI is toggleable

---
+++ Player Phases/Player Phases/Assets/Scripts/UI/ToggleUIPosition.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleUIPosition : MonoBehaviour
+{
+    public bool hiddenByDefault;
+
+    [Header("Movement")]
+    public RectTransform toMove;
+    public Vector3 moveBy;
+    public float lerpSpeed;
+    private Vector3 posRevealed;
+    private Vector3 posHidden;
+
+    private bool hidden = false;
+
+    void Start()
+    {
+        posRevealed = toMove.localPosition;
+        posHidden = toMove.localPosition + moveBy;
+
+        if (hiddenByDefault)
+        {
+            hidden = true;
+            toMove.localPosition = posHidden;
+        }
+    }
+
+    public void ToggleUI()
+    {
+        StopAllCoroutines();
+        hidden = !hidden;
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        if (hidden)
+        {
+            while (Vector2.Distance(toMove.localPosition, posHidden) > 1.0f)
+            {
+                toMove.localPosition = Vector2.Lerp(toMove.localPosition, posHidden, lerpSpeed);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        else
+        {
+            while (Vector2.Distance(toMove.localPosition, posRevealed) > 1.0f)
+            {
+                toMove.localPosition = Vector2.Lerp(toMove.localPosition, posRevealed, lerpSpeed);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
